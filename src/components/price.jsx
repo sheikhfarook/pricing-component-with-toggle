1e70ae4245b5d54100fbda3ffe6c8f26d899682f
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-05 14:55:23 +0530

    installed the chakra ui

--- src/components/price.jsx
+++ src/components/price.jsx
@@ -4,9 +4,9 @@ const Price = () => {
       <div>
         <h1>Our Pricing</h1>
         <div>
+          <div>Annually</div>
           <div></div>
-          <div></div>
-          <div></div>
+          <div>Monthly</div>
         </div>
       </div>
     </div>
